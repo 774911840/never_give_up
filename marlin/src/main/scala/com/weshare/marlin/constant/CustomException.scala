ba1e316627eca5e4a8abc0136aff9d6340545835
shaoshuai <774911840@qq.com>
2019-08-03 21:46:30 +0800

    a

--- marlin/src/main/scala/com/weshare/marlin/constant/CustomException.scala
+++
@@ -1,7 +0,0 @@
-package com.weshare.marlin.constant
-
-final case class CustomException(message: String = "",
-                                 cause: Throwable = None.orNull)
-  extends Exception(message, cause) {
-
-}
--- marlin/src/main/scala/com/weshare/marlin/logging/WeshareLogging.scala
+++
@@ -1,8 +0,0 @@
-package com.weshare.marlin.logging
-
-/**
-  * Created by xuyi on 2018/8/29 20:45.
-  */
-trait WeshareLogging {
-  protected lazy val logger: WeshareLogger = WeshareLogger()
-}
